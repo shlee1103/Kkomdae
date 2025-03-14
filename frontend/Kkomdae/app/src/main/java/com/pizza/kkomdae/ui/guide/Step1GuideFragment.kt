dea22950684f4d06bd1e4bb0a68e7122bff5a63b
이상혁 <leedltkdgur@naver.com>
2025-03-14 18:07:37 +0900

    feat: 외관 촬영 가이드 xml
    - 진행중
    
     진행중"
    S12P21D101-124 #in-progress

---
+++ frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/ui/guide/Step1GuideFragment.kt
@@ -0,0 +1,64 @@
+package com.pizza.kkomdae.ui.guide
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pizza.kkomdae.R
+import com.pizza.kkomdae.base.BaseFragment
+import com.pizza.kkomdae.databinding.FragmentLaptopInfoInputBinding
+import com.pizza.kkomdae.databinding.FragmentMainBinding
+import com.pizza.kkomdae.databinding.FragmentStep1GuideBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Step1GuideFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Step1GuideFragment : BaseFragment<FragmentStep1GuideBinding>(
+    FragmentStep1GuideBinding::bind,
+    R.layout.fragment_step1_guide
+){
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.topBar.tvStep.text = "외관 촬영 가이드"
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Step1GuideFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Step1GuideFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
