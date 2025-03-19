a09addafd45bf0f3bc0bf34b0abf160eeeca3c06
이송희 <thdgml1103@gmail.com>
2025-03-19 21:55:48 +0900

    feat: 스플래쉬에서 로그인 화면으로 이동 - 제일 첫 화면 스플래쉬화면으로 변경 - 로그인 꼬미 아이콘 변경 S12P21D101-179

--- frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/SplashActivity.kt
+++ frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/SplashActivity.kt
@@ -5,11 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
+        // Handler를 사용하여 일정 시간 후에 LoginActivity로 전환
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }, 2000)
     }
 }
