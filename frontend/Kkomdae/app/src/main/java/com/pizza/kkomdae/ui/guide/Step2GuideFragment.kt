65f1606d43db764d2a4395dd4d35af0255411769
이송희 <thdgml1103@gmail.com>
2025-03-21 17:32:27 +0900

    feat : Step 2 가이드 xml 개발 S12P21D101-125

--- frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/ui/guide/Step2GuideFragment.kt
+++ frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/ui/guide/Step2GuideFragment.kt
@@ -1,18 +1,14 @@
 package com.pizza.kkomdae.ui.guide
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.pizza.kkomdae.MainActivity
 import com.pizza.kkomdae.R
 import com.pizza.kkomdae.base.BaseFragment
-import com.pizza.kkomdae.databinding.FragmentLaptopInfoInputBinding
-import com.pizza.kkomdae.databinding.FragmentMainBinding
 import com.pizza.kkomdae.databinding.FragmentStep1GuideBinding
-private lateinit var mainActivity: MainActivity
+import com.pizza.kkomdae.databinding.FragmentStep2GuideBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,19 +17,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Step1GuideFragment.newInstance] factory method to
+ * Use the [Step2GuideFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Step1GuideFragment : BaseFragment<FragmentStep1GuideBinding>(
-    FragmentStep1GuideBinding::bind,
-    R.layout.fragment_step1_guide
-){
-
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        mainActivity = context as MainActivity
-    }
+class Step2GuideFragment : BaseFragment<FragmentStep2GuideBinding>(
+    FragmentStep2GuideBinding::bind,
+    R.layout.fragment_step2_guide
+) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,11 +39,7 @@ class Step1GuideFragment : BaseFragment<FragmentStep1GuideBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.topBar.tvTitle.text = "외관 촬영 가이드"
-        binding.topBar.pbStep.progress=100/3
-        binding.btnNext.setOnClickListener {
-            mainActivity.next()
-        }
-
+        binding.topBar.pbStep.progress=200/3
     }
 
     companion object {
@@ -63,12 +49,12 @@ class Step1GuideFragment : BaseFragment<FragmentStep1GuideBinding>(
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Step1GuideFragment.
+         * @return A new instance of fragment Step2GuideFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Step1GuideFragment().apply {
+            Step2GuideFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
