59d94abd1d67b61aaf372ff5cf7708603ca0e779
도승국 <dororo737@gmail.com>
2025-03-26 10:05:59 +0900

    docs: test환경을 prob으로 변경

--- backend/kkomdae/src/test/java/pizza/kkomdae/KkomdaeApplicationTests.java
+++ backend/kkomdae/src/test/java/pizza/kkomdae/KkomdaeApplicationTests.java
@@ -5,8 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
-//@ActiveProfiles("prod")
+//@ActiveProfiles("local")
+@ActiveProfiles("prod")
 class KkomdaeApplicationTests {
 
     @Test
