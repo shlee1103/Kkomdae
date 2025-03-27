4b88209352a7f1dba60daf17b910c467bd53113e
도승국 <dororo737@gmail.com>
2025-03-27 14:48:46 +0900

    fix: prod,local 변경

--- backend/kkomdae/src/main/java/pizza/kkomdae/controller/ApiController.java
+++ backend/kkomdae/src/main/java/pizza/kkomdae/controller/ApiController.java
@@ -94,12 +94,12 @@ public class ApiController {
         return testResultService.getPdfUrl(testId);
     }
 
-    @Operation(summary = "ai 사진 url update(파이썬 서버용)", description = "python용 s3 ai 이미지 업로드하고 url을 넣는 api")
-    @PostMapping("ai-photo")
-    public ApiResponse uploadAiPhoto(@RequestBody AiPhotoInfo aiPhotoInfo) {
-        photoService.uploadAiPhoto(aiPhotoInfo);
-        return new ApiResponse(true, "db에 s3 link 저장 성공");
-    }
+//    @Operation(summary = "ai 사진 url update(파이썬 서버용)", description = "python용 s3 ai 이미지 업로드하고 url을 넣는 api")
+//    @PostMapping("ai-photo")
+//    public ApiResponse uploadAiPhoto(@RequestBody AiPhotoInfo aiPhotoInfo) {
+//        photoService.uploadAiPhoto(aiPhotoInfo);
+//        return new ApiResponse(true, "db에 s3 link 저장 성공");
+//    }
 
     @Operation(summary = "qr 정보 입력", description = "2단계 qr 정보 입력 및 단계 저장")
     public void secondStage(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody SecondStageReq secondStageReq) {
