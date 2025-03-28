c0e2eefd61f7b6e6226c62b7fe346019362755f8
이송희 <thdgml1103@gmail.com>
2025-03-28 17:28:55 +0900

    feat: 컨페티 애니메이션 로직 추가 S12P21D101-191

---
+++ frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/ui/SubmitCompleteFragment.kt
@@ -0,0 +1,91 @@
+package com.pizza.kkomdae.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pizza.kkomdae.R
+import com.pizza.kkomdae.base.BaseFragment
+import com.pizza.kkomdae.databinding.FragmentOathBinding
+import com.pizza.kkomdae.databinding.FragmentSubmitCompleteBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SubmitCompleteFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SubmitCompleteFragment : BaseFragment<FragmentSubmitCompleteBinding>(
+    FragmentSubmitCompleteBinding::bind,
+    R.layout.fragment_submit_complete
+) {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 애니메이션
+        setupAnimation()
+
+        // 버튼 클릭 이벤트 설정
+        setupButtonListeners()
+    }
+
+    private fun setupAnimation() {
+        binding.animationView.setAnimation(R.raw.final_confetti)
+        binding.animationView.playAnimation()
+    }
+
+    private fun setupButtonListeners() {
+        // 닫기 버튼
+        binding.btnClose.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+
+        // PDF 다운로드 버튼
+        binding.btnDownloadPdf.setOnClickListener {
+            // PDF 다운로드 로직 구현
+        }
+
+        // 공유하기 버튼
+        binding.btnShare.setOnClickListener {
+            // 공유 로직 구현
+        }
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SubmitCompleteFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SubmitCompleteFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
