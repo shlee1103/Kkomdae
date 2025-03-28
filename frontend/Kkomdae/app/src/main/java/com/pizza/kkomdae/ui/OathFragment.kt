659d7ba31bdf465c4dfffb6f73fc7372bbb882d1
이송희 <thdgml1103@gmail.com>
2025-03-28 13:54:57 +0900

    design: step3 xml 수정 S12P21D101-193

--- frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/ui/OathFragment.kt
+++ frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/ui/OathFragment.kt
@@ -172,15 +172,6 @@ class OathFragment : BaseFragment<FragmentOathBinding>(
 
         }
 
-        // 다음 화면 기존 기기등록 화면으로 넘어가기
-//        binding.btnNext.setOnClickListener {
-//            if (isOath1Checked && isOath2Checked && isOath3Checked && isOath4Checked) {
-//                val transaction = requireActivity().supportFragmentManager.beginTransaction()
-//                transaction.replace(R.id.fl_main, AllStepGuideFragment())
-//                transaction.addToBackStack(null)
-//                transaction.commit()
-//            }
-//        }
         // 다음 화면 수정한 기기등록 화면으로 넘어가기
         binding.btnNext.setOnClickListener {
             if (isOath1Checked && isOath2Checked && isOath3Checked && isOath4Checked) {
