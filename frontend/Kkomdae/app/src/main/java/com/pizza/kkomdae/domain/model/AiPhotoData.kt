a5888f86a5bb03b188245aedf421a7c17ccbafba
이상혁 <leedltkdgur@naver.com>
2025-04-03 15:03:36 +0900

    feat: ai 결과 화면 데미지 수 추가
    Former-commit-id: 9508f2d52208eeecc31315f35413effffe845028

--- frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/domain/model/AiPhotoData.kt
+++ frontend/Kkomdae/app/src/main/java/com/pizza/kkomdae/domain/model/AiPhotoData.kt
@@ -13,4 +13,10 @@ data class AiPhotoData(
     val Picture5_ai_name: String?,
     val Picture6_ai_url: String?,
     val Picture6_ai_name: String?,
+    val photo1_ai_damage:Int?,
+    val photo2_ai_damage:Int?,
+    val photo3_ai_damage:Int?,
+    val photo4_ai_damage:Int?,
+    val photo5_ai_damage:Int?,
+    val photo6_ai_damage:Int?
 )
