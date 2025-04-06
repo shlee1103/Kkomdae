58b8e38594d2aacc1900a260fedb5a3d13918f1d
이송희 <thdgml1103@gmail.com>
2025-04-06 15:16:37 +0900

    design: footer 간격 조정
    Former-commit-id: 7cee7ad41950b0d02f64bcdbb2a6943211e078ed

--- frontend/kkomdae_landing/src/components/Footer.tsx
+++ frontend/kkomdae_landing/src/components/Footer.tsx
@@ -2,14 +2,10 @@ import "../styles/Footer.css";
 import axios from "axios";
 
 const Footer: React.FC = () => {
-
-
   const handleDownload = async () => {
     try {
       // 배포
-      const response = await axios.get(
-        `https://j12d101.p.ssafy.io/django/s3app/presigned-url/?file=kkomdae_diagnostics.exe`
-      );
+      const response = await axios.get(`https://j12d101.p.ssafy.io/django/s3app/presigned-url/?file=kkomdae_diagnostics.exe`);
 
       // 개발
       // const response = await axios.get(`http://127.0.0.1:8000/s3app/presigned-url/?file=kkomdae_diagnostics.exe`);
@@ -31,10 +27,7 @@ const Footer: React.FC = () => {
         <div className="footer-content">
           <div className="footer-logo">
             <span className="logo-text-footer">꼼대</span>
-            <p className="footer-description">
-              노트북 관리의 새로운 기준, 꼼대가 SSAFY 교육생 여러분의 노트북을
-              안전하게 지켜드립니다.
-            </p>
+            <p className="footer-description">노트북 관리의 새로운 기준, 꼼대가 SSAFY 교육생 여러분의 노트북을 안전하게 지켜드립니다.</p>
           </div>
 
           <div className="footer-links">
@@ -47,9 +40,6 @@ const Footer: React.FC = () => {
                 <li>
                   <a href="#how-it-works">이용 방법</a>
                 </li>
-                <li>
-                  <a href="#faq">자주 묻는 질문</a>
-                </li>
               </ul>
             </div>
 
@@ -57,10 +47,7 @@ const Footer: React.FC = () => {
               <h4 className="footer-links-title">다운로드</h4>
               <ul className="footer-links-list">
                 <li>
-                  <a href="#">iOS</a>
-                </li>
-                <li>
-                  <a href="#">Android</a>
+                  <a href="#">꼼대</a>
                 </li>
                 <li>
                   <button onClick={handleDownload} className="footer-modal-link">
@@ -73,9 +60,7 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="footer-bottom">
-          <p className="copyright">
-            © 2025 꼼대(KKOMDAE). All rights reserved.
-          </p>
+          <p className="copyright">© 2025 꼼대(KKOMDAE). All rights reserved.</p>
         </div>
       </div>
     </footer>
