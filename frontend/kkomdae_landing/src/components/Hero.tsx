20ab5f14737476f680a8ae578ca6b1d480b82431
이송희 <thdgml1103@gmail.com>
2025-04-07 13:14:40 +0900

    design: 프로그램 다운 ui 변경
    Former-commit-id: 6e6cabc8a236fbea531e721688862a6d89421fd7

--- frontend/kkomdae_landing/src/components/Hero.tsx
+++ frontend/kkomdae_landing/src/components/Hero.tsx
@@ -40,24 +40,10 @@ const Hero: React.FC = () => {
       alert("파일 다운로드 중 오류가 발생했습니다.");
     }
   };
-  // const handleAppDownload = async () => {
-  //   try {
-  //     // 배포
-  //     const response = await axios.get(`https://j12d101.p.ssafy.io/django/s3app/presigned-url/?file=kkomdae_diagnostics.exe`);
 
-  //     // 개발
-  //     // const response = await axios.get(`http://127.0.0.1:8000/s3app/presigned-url/?file=kkomdae_diagnostics.exe`);
-
-  //     const presignedUrl = response.data.url;
-  //     console.log(response);
-
-  //     // presigned URL로 브라우저를 이동하면 S3에서 직접 다운로드 시작
-  //     window.location.href = presignedUrl;
-  //   } catch (error) {
-  //     console.error(error);
-  //     alert("파일 다운로드 중 오류가 발생했습니다.");
-  //   }
-  // };
+  const handleAppDownload = () => {
+    window.location.href = "https://drive.usercontent.google.com/download?id=12UrM-tJEcpOQhEA5i8ou_QmuQEkXagLH&export=download&authuser=0";
+  };
 
   return (
     <section className="hero">
@@ -90,22 +76,10 @@ const Hero: React.FC = () => {
               </div>
             </div>
 
-            {/* <div className="hero-actions">
-              <a href="#diagnostic" className="btn-action btn-diagnostic">
-                자가진단 프로그램
-              </a>
-              <a href="#download" className="btn-action hero-download">
-                앱 다운로드
-              </a>
-            </div> */}
             <div className="download-buttons">
-              {/* Todo: handleAppDownload 수정 후 변경 필요 */}
-              <button onClick={handleSelfProgramDownload} className="download-btn">
+              <button onClick={handleAppDownload} className="download-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M3.00293 2.82257C2.83071 3.00921 2.74731 3.2743 2.74731 3.60694V20.9719C2.74731 21.3045 2.83071 21.5696 3.00293 21.7563L3.09523 21.8398L12.7614 12.3455V12.2267L3.09523 2.73911L3.00293 2.82257Z"></path>
-                  <path d="M16.9365 16.4329L12.7607 12.3455V12.2267L16.9373 8.14043L17.0482 8.20521L22.0011 10.9778C23.3653 11.7429 23.3653 12.8294 22.0011 13.5952L17.0482 16.3677L16.9365 16.4329Z"></path>
-                  <path d="M16.9366 16.3677L12.7608 12.2861L3.00226 21.7563C3.42251 22.2011 4.10367 22.2616 4.86457 21.8179L16.9366 16.3677Z"></path>
-                  <path d="M16.9366 8.14036L4.86457 2.69011C4.10367 2.24636 3.42251 2.3076 3.00226 2.75239L12.7608 12.2267L16.9366 8.14036Z"></path>
+                  <path d="M17 2H7c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-5 18c-.83 0-1.5-.67-1.5-1.5S11.17 17 12 17s1.5.67 1.5 1.5S12.83 20 12 20zm5-4H7V4h10v12z" />
                 </svg>
                 <span>
                   <small>다운로드</small>꼼대 설치하기
@@ -113,8 +87,7 @@ const Hero: React.FC = () => {
               </button>
               <button onClick={handleSelfProgramDownload} className="download-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M17.1839 12.0371C17.1716 9.53799 19.2531 8.16724 19.3305 8.11683C17.9878 6.16301 15.888 5.94993 15.1396 5.92358C13.4343 5.74686 11.776 6.91333 10.9155 6.91333C10.0259 6.91333 8.66613 5.94407 7.25105 5.97628C5.39358 6.00849 3.6873 7.05773 2.7499 8.65255C0.820501 11.8992 2.22999 16.679 4.07453 19.136C5.00311 20.338 6.09098 21.67 7.48578 21.6183C8.82815 21.5626 9.31498 20.705 10.9155 20.705C12.4988 20.705 12.951 21.6183 14.3633 21.5861C15.8069 21.5626 16.7633 20.3864 17.6674 19.168C18.7357 17.783 19.1606 16.4317 19.1758 16.3721C19.14 16.3584 17.1976 15.5806 17.1839 12.0371Z"></path>
-                  <path d="M14.5555 4.05063C15.3023 3.1342 15.8028 1.86541 15.6715 0.572266C14.5798 0.615248 13.2466 1.29354 12.4695 2.17864C11.7873 2.95551 11.1848 4.28202 11.3283 5.51672C12.5597 5.61167 13.7783 4.93865 14.5555 4.05063Z"></path>
+                  <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" />
                 </svg>
                 <span>
                   <small>다운로드</small>
