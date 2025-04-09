e75c23b4c0a9615c21524135152490f9b50a8ae1
김성수 <sskim629@gmail.com>
2025-04-09 10:45:34 +0900

    fix : 반납 pdf에서 이전 대여일자, 대여시 수령한 물품 수량을 추가
    Former-commit-id: a9223286ac6864b56ae0893295dad529aecdc0dc

--- backend/kkomdae/src/main/java/pizza/kkomdae/dto/PdfInfo.java
+++ backend/kkomdae/src/main/java/pizza/kkomdae/dto/PdfInfo.java
@@ -27,8 +27,15 @@ public class PdfInfo {
     private LocalDate returnDate;
     private LocalDate rentDate;
     private List<Photo>photos;
+    private boolean release;
+    private int rentLaptopCount;
+    private int rentPowerCableCount;
+    private int rentAdapterCount;
+    private int rentMouseCount;
+    private int rentBagCount;
+    private int rentMousePadCount;
 
-    public PdfInfo(LaptopTestResult result) {
+    public PdfInfo(LaptopTestResult result, LaptopTestResult rent) {
         Student student = result.getStudent();
         this.name = student.getName();
         this.region = student.getRegion();
@@ -44,13 +51,22 @@ public class PdfInfo {
         this.bagCount = result.getBag();
         this.mousePadCount = result.getBag();
         this.photos = result.getPhotos();
+        this.release=result.getRelease();
         if (result.getRelease()) {
+            this.rentDate = rent.getDate();
             this.returnDate = result.getDate();
-            log.info("returnDate : {}",this.rentDate);
+            log.info("rentDate : {}",this.rentDate);
+            log.info("returnDate : {}",this.returnDate);
         }else{
             this.rentDate = result.getDate();
             log.info("rentDate : {}",this.rentDate);
         }
         this.description = result.getDescription();
+        this.rentMousePadCount = rent.getMousePad();
+        this.rentMouseCount = rent.getMouse();
+        this.rentBagCount = rent.getBag();
+        this.rentAdapterCount = rent.getAdapter();
+        this.rentLaptopCount = rent.getLaptop();
+        this.rentPowerCableCount = rent.getPowerCable();
     }
 }
